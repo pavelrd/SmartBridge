c7d58754b254c3c5a390e5ae6463006876704058
Pavel <pavel3593@yandex.ru>
2024-11-01 22:24:39 +0300

    Added test for temperature(check sensor data available, room temperatures check)

--- uart.py
+++ uart.py
@@ -11,6 +11,18 @@ ser = serial.Serial(port, baudrate=baudrate)
 
 print("Serial ok")
 
+def testThermal(name, telemetryJson):
+    if name in telemetryJson:
+        print("---->",telemetryJson["t0"])
+        if (telemetryJson[name] < 20) or (telemetryJson["t0"] > 30):
+            print("ERRROR!!!!!")
+            exit(-1)
+        else:
+            print("T0 - ",telemetryJson["t0"])
+    else:
+        print("Temperature ERROR")
+        exit(-1)
+
 def testfunc(one, two):
     if random.randint(0, 1): 
         ser.write(one.encode('UTF-8'))
@@ -23,12 +35,20 @@ counter = 0
 
 while True:
     print("Test num: ", counter)
-    time.sleep(0.5)
+    #time.sleep(1)
+    
+    ser.write('a'.encode('UTF-8'))
+    
+    reasonJson = json.loads(ser.readline())
+    
+    print(reasonJson)
+    
+    
     ser.write('g'.encode('UTF-8'))
     print("-------")
     
     telemetryData    = ser.readline()
-    
+    print(telemetryData)
     telemetryJson    = json.loads(telemetryData)
     telemetryJsonCrc = json.loads(ser.readline())
     
@@ -36,22 +56,24 @@ while True:
     crcFromTelemetry = telemetryJsonCrc["crc"]
     crcCalculated    = zlib.crc32(telemetryData[:-2])
     
-    print(telemetryJson)
+    # print(telemetryJson)
     
     if crcFromTelemetry != crcCalculated:
         print("ERROR BAD CRC!!!!!")
         exit(-1)
     
-    print("-------")
-    ser.write('a'.encode('UTF-8'))
+    testThermal("t0", telemetryJson)
+    testThermal("t1", telemetryJson)
+    testThermal("t2", telemetryJson)
+    testThermal("t3", telemetryJson)
+    testThermal("t4", telemetryJson)
     
-    reasonJson = json.loads(ser.readline())
-    
-    print(reasonJson)
+    #print("-------")
+
     
     if reasonJson["last_reset_reasons"] != 1:
         print("ERROR RESETTED!")
-        exit(-1)
+        #exit(-1)
     
     print("-------")
    
